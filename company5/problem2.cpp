27be67677060afb8962b79e16d8b8e36f12679d3
Vindhya-14 <vindhya3321@gmail.com>
2025-01-26 08:33:02 +0530

    solved company5  problem2

---
+++ company5/problem2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int  n = nums2.size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        int ans = 0;
+        for(int i=1;i<m+1;i++)
+        {
+            for(int j=1;j<n+1;j++)
+            {
+                if(nums1[i-1] == nums2[j-1])
+                {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = 0;
+                }
+
+                ans = max(ans,dp[i][j]);
+            }
+        }
+        return ans;
+    }
+};
