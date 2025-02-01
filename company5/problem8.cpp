588dbd108e005c16d4d2a0a048f853fa73b304f0
Vindhya-14 <vindhya3321@gmail.com>
2025-02-01 18:16:56 +0530

    solve company5 problem8

---
+++ company5/problem8.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<string>  result;
+    void solve(int idx,string &digits , unordered_map<char,string> &mp , string &temp)
+    {
+          if (idx >= digits.length()) {
+            result.push_back(temp);
+            return;
+        }
+
+        char ch = digits[idx];
+        string str = mp[ch]; 
+
+        for (int i = 0; i < str.size(); i++) { 
+            temp.push_back(str[i]);
+            solve(idx + 1, digits, mp, temp);
+            temp.pop_back();
+        }
+
+    }
+   
+    vector<string> letterCombinations(string digits) {
+        if(digits.length() == 0)
+        {
+            return {};
+        }
+        unordered_map<char,string> mp;
+        mp['2']= "abc";
+        mp['3']= "def";
+        mp['4']= "ghi";
+        mp['5']= "jkl";
+        mp['6']= "mno";
+        mp['7']= "pqrs";
+        mp['8']= "tuv";
+        mp['9']= "wxyz";
+
+        string temp = "";
+        solve(0,digits, mp,temp);
+        return result;
+    }
+};
